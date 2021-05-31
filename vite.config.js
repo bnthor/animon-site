40a9fc52bbfb5b47aed0cffb6c6f8eb256fe54d4
Benjamin Réthoré <ben@rethore.im>
2021-05-31 11:43:37 +0200

    Adds production specifig config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
-export default {
-  root: 'src/',
-  build: {
-    outDir: '../'
-  }
+export default ({ command, mode }) => {
+    if (command === 'serve') {
+        return {
+            root: 'src/',
+            build: {
+                outDir: '../'
+            }
+        }  
+    } else {
+        return {
+            root: 'src/',
+            base: '/animon-site',
+            build: {
+                outDir: '../'
+            }
+        }  
+    } 
 }
